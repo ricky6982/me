e5751fc6690301c24a1f2a33ac58ae1d3c41bde9
RickySSD <ricky6982@gmail.com>
2015-09-17 01:52:01 -0300

    Primera configuración de ui-router.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-var app = angular.module('app', ['ui-router']);
+var app = angular.module('app', ['ui.router']);
 
-app.config(function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwhise('/home');
-    
-});
+app.config([
+    '$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home', {
+                url: '/home'
+            })
+        ;
+    }
+]);
