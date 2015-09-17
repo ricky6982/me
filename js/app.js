5b2d958705c2bd271b6ace1d4f81d27c8415c7d7
RickySSD <ricky6982@gmail.com>
2015-09-17 01:00:31 -0300

    Se corrigio path del componente angular-ui-router.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui-router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwhise('/home');
+    
+});
